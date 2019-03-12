4e3ebd5b38e9eb69282fc354b796b871f4995449
caseydulong <caseydulong@gmail.com>
2019-03-12 18:23:08 -0400

    Adds simple time to events serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class EventSerializer < ActiveModel::Serializer
   attributes :id,
              :name,
@@ -6,5 +8,15 @@ class EventSerializer < ActiveModel::Serializer
              :start_time,
              :end_time,
              :location,
-             :user_id
+             :user_id,
+             :simple_start_time,
+             :simple_end_time
+
+  def simple_start_time
+    object.start_time.to_s[11..15]
+  end
+
+  def simple_end_time
+    object.end_time.to_s[11..15]
+  end
 end
