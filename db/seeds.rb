3e1f3144fed46842bfc9c84ed5a54abb0e3e66e0
caseydulong <caseydulong@gmail.com>
2019-03-12 16:02:02 -0400

    Updates seeds.rb with more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,65 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+  email: 'miles@knight.com',
+  password: '123',
+  password_confirmation: '123'
+)
+User.create(
+  email: 'tia@snake.com',
+  password: '123',
+  password_confirmation: '123'
+)
+User.create(
+  email: 'user1@email.com',
+  password: '123',
+  password_confirmation: '123'
+)
+User.create(
+  email: 'user2@email.com',
+  password: '123',
+  password_confirmation: '123'
+)
+User.create(
+  email: 'user3@email.com',
+  password: '123',
+  password_confirmation: '123'
+)
+
+Event.create(
+  name: 'Miles\' birthday',
+  start_date: Date.parse('2019-02-25'),
+  end_date: Date.parse('2019-02-25'),
+  user_id: 2
+)
+Event.create(
+  name: 'Camping trip',
+  start_date: Date.parse('2019-07-04'),
+  end_date: Date.parse('2019-07-06'),
+  location: 'New Hampshire',
+  user_id: 2
+)
+Event.create(
+  name: 'Tia\'s birthday',
+  start_date: Date.parse('2019-01-31'),
+  end_date: Date.parse('2019-01-31'),
+  user_id: 2
+)
+Event.create(
+  name: 'Sleepover',
+  start_date: Date.parse('2019-03-15'),
+  end_date: Date.parse('2019-03-16'),
+  start_time: Time.parse('4:00 pm'),
+  end_time: Time.parse('3:00 pm'),
+  location: 'Miles\' house',
+  user_id: 2
+)
+Event.create(
+  name: 'Dinner party',
+  start_date: Date.parse('2019-03-15'),
+  end_date: Date.parse('2019-03-15'),
+  start_time: Time.parse('7:30 pm'),
+  location: 'Tia\'s belly',
+  user_id: 2
+)
