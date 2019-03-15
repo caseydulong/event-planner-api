dc940f8d787d770f65d026da658629b6aeb37b59
caseydulong <caseydulong@gmail.com>
2019-03-14 23:40:36 -0400

    Updates date format in serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -10,7 +10,9 @@ class EventSerializer < ActiveModel::Serializer
              :location,
              :user_id,
              :simple_start_time,
-             :simple_end_time
+             :simple_end_time,
+             :format_start_date,
+             :format_end_date
 
   def simple_start_time
     object.start_time.to_s[11..15]
@@ -18,5 +20,19 @@ class EventSerializer < ActiveModel::Serializer
 
   def simple_end_time
     object.end_time.to_s[11..15]
+  end
+
+  def format_start_date
+    year = object.start_date.to_s[0..3]
+    month = object.start_date.to_s[5..6]
+    day = object.start_date.to_s[8..9]
+    date = "#{month}/#{day}/#{year}"
+  end
+
+  def format_end_date
+    year = object.end_date.to_s[0..3]
+    month = object.end_date.to_s[5..6]
+    day = object.end_date.to_s[8..9]
+    date = "#{month}/#{day}/#{year}"
   end
 end
